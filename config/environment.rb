fd4afd680dc0703b4742e85cfa579044c9f9705f
Anthony Underwood <email2ants@gmail.com>
2009-08-02 09:58:21 +0100

    add gems to environment

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "be9-acl9", :source => "http://gems.github.com", :lib => "acl9"
+	config.gem "thoughtbot-clearance", :lib     => 'clearance', :source  => 'http://gems.github.com'
+
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
