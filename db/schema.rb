ef52f817b347fdb2ec6bd9f1a1e4e38e31fe6009
Anthony Underwood <email2ants@gmail.com>
2009-08-02 23:24:31 +0100

    add authlogic functionality

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090731162828) do
+ActiveRecord::Schema.define(:version => 20090802214316) do
 
   create_table "secret_messages", :force => true do |t|
     t.string   "title"
-    t.text     "content"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
