de641625d25b7df663b91cafd595eb08d0d3b59b
Anthony Underwood <email2ants@gmail.com>
2009-08-04 14:16:23 +0100

    refine css styling

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "page", :name => "welcome", :action =>"show"
+  map.root :controller => "user_sessions", :action =>"new"
 
   # See how all your routes lay out with "rake routes"
 
