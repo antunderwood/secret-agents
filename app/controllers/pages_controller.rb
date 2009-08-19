56bc259e6ae1507a7c64882d9c81963e1e7d70a0
Anthony Underwood <email2ants@gmail.com>
2009-08-19 20:04:00 +0100

    tying down videos permissions and addling link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,10 @@
 class PagesController < HighVoltage::PagesController
   access_control do
     allow logged_in
-    deny anonymous,  :to => :show, :unless => :public_page
+    allow anonymous,  :to => :show, :if => :public_page
   end
   private
   def public_page
-    return false
-    true if ["videos"].include?(params[:id])
+    true if ["welcome"].include?(params[:id])
   end
 end
